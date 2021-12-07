a3c086f46fae3c96e7f310a350db921ea2cf3cbe
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-08 01:31:26 +1000

    Добавьте файлы проекта.

---
+++ Lesson21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создать метот расширения для стандартного типа данных
+            //Создать метот расширения для интерфейса
+            //*Создать метод расширения для sealed-класса из внешней библиотеки
+        }
+    }
+}
