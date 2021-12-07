c20c20128ccb4bd6471fd32bbd5f1f7469753ab9
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-08 02:12:12 +1000

    Добавил класс ToSrting

---
+++ Lesson21/ToStingCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21
+{
+    public static class ToStingCollection
+    {
+        public static string ConvertToString(this IEnumerable collection)
+        {
+            var result = " ";
+
+            foreach (var item in collection)
+            {
+                result += item.ToString() + ", ";
+            }
+
+            return result;
+        }
+    }
+}
