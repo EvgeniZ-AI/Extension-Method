6c03983a4f21056c02a0eaf5d70ee0b15ffe23f9
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-08 02:17:49 +1000

    Добавил класс поставки угля

---
+++ Lesson21/amountOfCoal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21
+{
+    public sealed class amountOfCoal
+    {
+        public string num { get; set; }
+        public int amount { get; set; }
+
+        public override string ToString()
+        {
+            return $"Номер поставки: {num}. Коллиечство угля в поставке: {amount}";
+        }
+    }
+}
