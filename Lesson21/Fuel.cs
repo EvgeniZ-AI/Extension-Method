568ed1b21c357e56c374ec0320749c2d519b286a
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-08 01:53:22 +1000

    зоздал класс угя

---
+++ Lesson21/Fuel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Lesson21
+{
+    public static class Fuel
+    {
+        public static int isCoal(this int i)
+        {
+            return i * 7 ; //энергия в кВт-ч
+        }
+    }
+}
