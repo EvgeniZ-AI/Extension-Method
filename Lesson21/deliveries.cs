762c19a042105867ffbfb293c8d7a5c5c31aff89
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-08 02:35:11 +1000

    Добавил класс рандомайзер поставок угля

---
+++ Lesson21/deliveries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21
+{
+    public static class deliveries
+    {
+        public static amountOfCoal createRandomDeliveris(this amountOfCoal amountCoal, int min,int max)
+        {
+            var rnd = new Random(DateTime.Now.Millisecond);
+            amountCoal.num = "№" + rnd.Next(1, 250);
+            amountCoal.amount = rnd.Next(min, max);
+
+            return amountCoal;
+        }
+    }
+}
